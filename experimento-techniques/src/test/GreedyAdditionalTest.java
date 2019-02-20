e2b34c0c054b7c54b4ca87bba87cff79a2b658b3
Wesley Nunes Marques Torres <wesley.nmtorres@gmail.com>
2019-02-20 19:42:47 -0300

    additional similarity experiment

--- experimento-techniques/src/test/GreedyAdditionalTest.java
+++ experimento-techniques/src/test/GreedyAdditionalTest.java
@@ -6,17 +6,16 @@ import main.GreedyAdditionalSimilarity;
 
 public class GreedyAdditionalTest {
 	
-	private final String directory = "/Users/wesley/Documents/workspaceJava/experimento-techniques/src/test/resources";
-	private final String matrixFile = "method";
-	private final double similarityFactor = 0.5;
-	private final double testSimilar2Move = 0.5;
+	private final String directory = "/Users/wesley/Documents/workspaceMastering/tcp-experiment/data/scribe-java/coverage/9cb2af37c7c3f9fdcc3da63714ba801747df33b2/";
+	private final String matrixFile = "statement";
+	private final double similarityFactor = 0.05;
+	private final double testSimilar2Move = 0.05;
 	
 	GreedyAdditionalSimilarity gAddSimilarity;
 	
 
 	@Test
 	public void test() {
-		
 		gAddSimilarity = new GreedyAdditionalSimilarity(this.directory, this.matrixFile, (float)this.similarityFactor, (float)this.testSimilar2Move);
 		gAddSimilarity.Print(gAddSimilarity.getSelectedTestSequence());
 	}
